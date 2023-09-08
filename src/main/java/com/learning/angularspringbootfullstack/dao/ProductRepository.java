eab83dae6a2a5be64e7b03e80b9b1ae28d59d4ef
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-09-08 17:00:45 +0530

    added CORS support

--- src/main/java/com/learning/angularspringbootfullstack/dao/ProductRepository.java
+++ src/main/java/com/learning/angularspringbootfullstack/dao/ProductRepository.java
@@ -2,7 +2,8 @@ package com.learning.angularspringbootfullstack.dao;
 
 import com.learning.angularspringbootfullstack.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
