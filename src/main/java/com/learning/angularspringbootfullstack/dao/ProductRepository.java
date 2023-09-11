6dc79e1d90e83e45e78f9dd9025b3d0f14c2d670
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-09-11 14:59:12 +0530

    added search by keyword functionality

--- src/main/java/com/learning/angularspringbootfullstack/dao/ProductRepository.java
+++ src/main/java/com/learning/angularspringbootfullstack/dao/ProductRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Page<Product> findProductByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+    Page<Product> findProductByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
+    Page<Product> findByDescriptionContainsIgnoreCase(@Param("value") String value, Pageable pageable);
 }
